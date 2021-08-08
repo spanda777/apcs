882192a720442cd595bad78583999070a819e3f9
Shruti Panda <shrutipanda777@gmail.com>
2021-08-08 11:34:39 -0500

    initial commit

---
+++ First.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class First here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class First
+{
+    public static void main(String[] args){
+        System.out.println(args.length);
+        for (String i : args){
+            System.out.println(i.toUpperCase());
+        }
+        System.out.println("Hello World");
+        }
+    }
+    
+
---
+++ Second.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class First here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Second
+{
+    public static void main(String[] args){
+         
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[args.length-i-1]);
+        }
+        
+        }
+    }
+    
+
